bf7bc4ecac0d2c82fc1ae7bf4f4f5b359be9c986
Nicholas Tay <nexerq@gmail.com>
2015-08-31 23:02:48 +1000

    New raw prototype to take JS objects over string

--- core.js
+++ core.js
@@ -14,7 +14,7 @@ function OsuApi (options) {
   this.settings = settings;
 };
 
-OsuApi.prototype.raw = function (endpoint, params, cb) {
+OsuApi.prototype.rawString = function (endpoint, params, cb) { // Raw by string
   var url = this.settings.apiUri + endpoint + '?k=' + this.settings.apiKey + '&' + params;
 
   request(url, function (err, response, body) {
@@ -25,5 +25,33 @@ OsuApi.prototype.raw = function (endpoint, params, cb) {
     return cb(err);
   });
 };
+
+OsuApi.prototype.raw = function (endpoint, params, cb) { // Raw by JavaScript object. Main basis of other convenience functions
+  var options = {
+    k: this.settings.apiKey
+  };
+
+  var options = extend(options, params);
+
+  // Construct the url then make request
+  var url = this.settings.apiUri + endpoint + '?';
+  // https://stackoverflow.com/questions/684672/loop-through-javascript-object
+  for (var key in options) {
+    if (options.hasOwnProperty(key)) {
+      url += (key + "=" + options[key] + "&");
+    };
+  };
+
+  request(url, function (err, response, body) {
+    if (!err && response.statusCode == 200) {
+      return cb(null, body);
+    };
+
+    return cb(err);
+  });
+};
+
+OsuApi.prototype.modes = require('./api/modes.js');
+OsuApi.prototype.beatmaps = require('./api/beatmap.js');
 
 module.exports = OsuApi;
--- example.js
+++ example.js
@@ -4,7 +4,15 @@ var osu = new OsuApi({
   apiKey: 'your_key_here'
 });
 
-osu.raw('/get_user', 'u=Nexerq', function(err, response) {
+// osu.rawString('/get_user', 'u=Nexerq', function(err, response) {
+//   if (!err) {
+//     console.log(response);
+//     return;
+//   };
+//   console.log(err);
+// });
+
+osu.raw('/get_user', {u: "Nexerq"}, function(err, response) {
   if (!err) {
     console.log(response);
     return;
