90504e7b861802c1e625e7a217fc58baa9832b17
Nicholas Tay <nexerq@gmail.com>
2015-08-31 22:24:42 +1000

    Better way of doing settings

--- core.js
+++ core.js
@@ -1,12 +1,21 @@
 var request = require('request')
+var extend = require('xtend') // Use simple extend module because I'm lazy to write my own
 
-function OsuApi (apiKey) {
-  this.apiUri = 'https://osu.ppy.sh/api'
-  this.apiKey = apiKey;
+function OsuApi (options) {
+  // Bunch of default settings
+  var settings = {
+    apiUri: 'https://osu.ppy.sh/api',
+    apiKey: null
+  };
+
+  // "Extend" the user's ones in
+  settings = extend(settings, options)
+
+  this.settings = settings;
 };
 
 OsuApi.prototype.raw = function (endpoint, params, cb) {
-  var url = this.apiUri + endpoint + '?k=' + this.apiKey + '&' + params;
+  var url = this.settings.apiUri + endpoint + '?k=' + this.settings.apiKey + '&' + params;
 
   request(url, function (err, response, body) {
     if (!err && response.statusCode == 200) {
--- example.js
+++ example.js
@@ -1,9 +1,13 @@
 var OsuApi = require('./index.js');
 
-var osu = new OsuApi('your_api_key');
+var osu = new OsuApi({
+  apiKey: 'your_key_here'
+});
 
 osu.raw('/get_user', 'u=Nexerq', function(err, response) {
   if (!err) {
     console.log(response)
+    return;
   }
+  console.log(err)
 });
