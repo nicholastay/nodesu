c9d71b0c00e67c4536cf0c87338b63c0742eb6ba
Nicholas Tay <nexerq@gmail.com>
2015-09-03 15:51:35 +1000

    Forgot to commit get_scores ;)

---
+++ examples/scores_request.js
@@ -0,0 +1,18 @@
+var OsuApi = require('../lib/index.js');
+
+var osu = new OsuApi({
+  apiKey: 'your_api_key_here'
+});
+
+osu.getScores(osu.beatmap.byMapID(546514), osu.scores.all, osu.mode.all, function(err, response) {
+  if (err) {
+    return err;
+  }
+
+  console.log("Raw output:");
+  console.log(JSON.stringify(response) + "\n");
+  console.log("Parsed response:");
+  var parseResp = "Rank #1 globally on this map: " + response[0].username + "\nScore: " + response[0].score + "\nTimestamp achieved:" + response[0].date;
+  console.log(parseResp);
+  return;
+});
