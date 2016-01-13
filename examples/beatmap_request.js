1e8b9e4e583f42a3c4331842b0f9c785cf34d6c2
Nicholas Tay <nexerq@gmail.com>
2016-01-13 22:50:18 +1100

    0.3.0 - Promises with Q, fixing linting issues

--- examples/beatmap_request.js
+++ examples/beatmap_request.js
@@ -9,15 +9,37 @@ function round(value, decimals) {
     return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
 
-osuApi.getBeatmaps(osuApi.beatmap.byMapset(222428), osuApi.mode.all, function(err, response) {
-  if (err) {
-    return console.log (err);
-  }
+function getByCallback(cb) {
+  osuApi.getBeatmaps(osuApi.beatmap.byMapset(222428), osuApi.mode.all, function(err, response) {
+    if (err) {
+      return console.log("Callback response err: " + err);
+    }
 
-  console.log("Raw output:");
-  console.log(JSON.stringify(response) + "\n");
-  console.log("Parsed response:");
-  var parseResp = "Song name: " + response[0].title + "\n" + "Artist name: " + response[0].artist + "\n" + "Mapped by: " + response[0].creator + "\n" + "[" + response[0].version + "] (" + response[0].bpm + "BPM" + ") " + round(response[0].difficultyrating, 2) + "★";
-  console.log(parseResp);
-  return;
-});
+    console.log("Callback response: ");
+    console.log("Raw output:");
+    console.log(JSON.stringify(response) + "\n");
+    console.log("Parsed response:");
+    var parseResp = "Song name: " + response[0].title + "\n" + "Artist name: " + response[0].artist + "\n" + "Mapped by: " + response[0].creator + "\n" + "[" + response[0].version + "] (" + response[0].bpm + "BPM" + ") " + round(response[0].difficultyrating, 2) + "★";
+    console.log(parseResp);
+    return cb();
+  });
+}
+
+function getByPromise() {
+  osuApi.getBeatmaps(osuApi.beatmap.byMapset(83560), osuApi.mode.all)
+  .then(function(response) {
+    console.log("Promise response: ");
+    console.log("Raw output:");
+    console.log(JSON.stringify(response) + "\n");
+    console.log("Parsed response:");
+    var parseResp = "Song name: " + response[0].title + "\n" + "Artist name: " + response[0].artist + "\n" + "Mapped by: " + response[0].creator + "\n" + "[" + response[0].version + "] (" + response[0].bpm + "BPM" + ") " + round(response[0].difficultyrating, 2) + "★";
+    console.log(parseResp);
+    return;
+  })
+  .catch(function(err) {
+    console.log("Promise response err: " + err)
+  });
+}
+
+// CB first then PROMISE
+getByCallback(function() {getByPromise()});
