eec9f527740430a522e8abefca65ffbe85995dfd
Nicholas Tay <nexerq@gmail.com>
2016-09-09 22:39:13 +1000

    Add user class and cleanup beatmap

--- nodesu.js
+++ nodesu.js
@@ -11,3 +11,8 @@ exports.Mods = require('./lib/enum/Mods');
 exports.ApprovalStatus = require('./lib/enum/ApprovalStatus');
 exports.Genre = require('./lib/enum/Genre');
 exports.Language = require('./lib/enum/Language');
+
+// classes
+exports.Beatmap = require('./lib/struct/Beatmap');
+exports.User = require('./lib/struct/User');
+exports.UserEvent = require('./lib/struct/UserEvent');
